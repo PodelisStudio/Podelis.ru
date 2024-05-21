d641fcdbaefac80017564a810fd4ea6bd2f772c3
PodelisStudio <165757415+PodelisStudio@users.noreply.github.com>
2024-05-21 16:42:36 +1000

    Add files via upload

--- assets/JS/anime.js
+++ assets/JS/anime.js
@@ -2,24 +2,42 @@ let videoList = document.querySelector(".video-grid");
 
 let massListAnime = [
   {
-    title: "Аниме 1",
-    src: "",
+    title: "pedro",
+    src: "./assets/video/pedro.mp4",
     description: "Аниме титл",
     poster: "./assets/img/Anime/hunt.jpg",
   },
   {
     title: "Аниме 2",
-    src: "",
+    src:"./assets/video/hunt1.mp4",
     description: "Аниме титл 2",
     poster: "./assets/img/Anime/2х.png",
   },
+  
+  {
+    title: "Аниме 2",
+    src: "./assets/video/runhunt.mp4",
+    description: "Аниме титл 2",
+    poster: "./assets/img/Anime/2х.png",
+  },
+  {
+    title: "Аниме 2",
+    src: "./assets/video/huntedit.mp4",
+    description: "Аниме титл 2",
+    poster: "./assets/img/Anime/2х.png",
+  },
+
+
 ];
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < massListAnime.length; i++) {
   let video = document.createElement("video");
   video.poster = massListAnime[i].poster;
 
   console.log(videoList);
   videoList.insertAdjacentElement("afterbegin", video);
   video.className = "video";
+  video.src = massListAnime[i].src;
+  video.controls = true
 }
+console.log(massListAnime);
